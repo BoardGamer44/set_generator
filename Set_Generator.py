55383757f45755fae7c8cb99af796e5c81b37981
BoardGamer44 <61357119+BoardGamer44@users.noreply.github.com>
2020-03-30 23:41:37 +0300

    Update Set_Generator.py
    Функции generate_set, set_gradients, set_patterns вынесены в отдельные файлы.
    Перекомпоновка кода
    Добавлена переменная folder_path для задания папки генерации файлов, имя файлов будет фиксированным

--- Set_Generator.py
+++ Set_Generator.py
@@ -1,77 +1,25 @@
 import svgwrite
 
-def generate_set(feature_number=4,feature_variety=4):
-#    функция генерирует массив кодов, определяющих изображение на картах, т.к. для целей игры
-#    вариативность каждого признака должна быть одинаковой - процесс похож на перечисление всех чисел
-#    системы счисления с основанием feature_variety (по умолчанию - троичная система счисления)
-#    feature_number число признаков(разрядность, возможные варианты - 4(по умолчанию) и 3(только зеленый цвет))
-#    feature_variety вариативность признаков(основание системы счисления, минимум 2, 
-#   максимум - максимальное число методов в классе Card(пока 3), теоритический максимум алгоритма - 9)
-    code_of_set_list=[0 for i in range(feature_variety**feature_number)] # массив кодов(число возможных вариаций чисел в заданной разрядности)
-    append_list=[0 for i in range(feature_number)] # массив приращений(нужен для работы механизма переноса разряда)
-    start_code_value=int('1'*feature_number) # стартовое значение(нулевое число системы счисления с учетом разрядов, в данном случае - 1111)
-    code_counter=0 # счетчик массива кодов
-    while feature_variety**feature_number>code_counter: # цикл четырехразрядного приращения feature_variety-ричной системы счисления
-        # выполнять пока не достигнуто максимальное число комбинаций(максимальное число в системе отсчета)
-        code_of_set_list[code_counter]=start_code_value
-        for i in range(feature_number): # цикл приращения
-            code_of_set_list[code_counter]+=append_list[i] 
-        code_counter+=1
-        append_list[feature_number-1]+=1
-        for j in range(feature_number-1): # цикл переноса разряда с права на лево
-            if append_list[feature_number-1-j]==feature_variety*(int(round(0.1**(-j), 1))):
-                append_list[feature_number-1-j]=0
-                append_list[feature_number-2-j]+=(int(round(0.1**(-j-1), 1)))
-    return code_of_set_list
+# функция генерирует массив кодов
+from generate_set import generate_set
 
-# папка и название первого файла, обязательно с нулем в конце
-file_path='C:\\test0.svg'
+# создание градиентов и паттернов
+from set_gradients import set_gradients
+from set_patterns import set_patterns
+
+# папка назначения
+folder_path='C:\\print\\'
+file_path=folder_path+'print0.svg'
 dwg = svgwrite.Drawing(file_path, size=(u'100%', u'100%'))
 
-# градиенты вынесены отдельно так как непонятно как создать 
-# отдельный объект градиента в каждом объекте карты
-# сейчас они пересоздаются при каждом открытии файла
-def set_gradients():
-    rad_grad1=svgwrite.gradients.RadialGradient(id="grad1")
-    rad_grad1.add_stop_color(offset='0%', color='rgb(230, 25, 25)', opacity=None)
-    rad_grad1.add_stop_color(offset='100%', color='white', opacity=None)
-    dwg.defs.add(rad_grad1)
-    
-    rad_grad2=svgwrite.gradients.RadialGradient(id="grad2")
-    rad_grad2.add_stop_color(offset='0%', color='rgb(50, 165, 80)', opacity=None)
-    rad_grad2.add_stop_color(offset='100%', color='white', opacity=None)
-    dwg.defs.add(rad_grad2)
-    
-    rad_grad3=svgwrite.gradients.RadialGradient(id="grad3")
-    rad_grad3.add_stop_color(offset='0%', color='rgb(20, 50, 250)', opacity=None)
-    rad_grad3.add_stop_color(offset='100%', color='white', opacity=None)
-    dwg.defs.add(rad_grad3)
-    
-    rad_grad4=svgwrite.gradients.RadialGradient(id="grad4")
-    rad_grad4.add_stop_color(offset='0%', color='rgb(0, 0, 0)', opacity=None)
-    rad_grad4.add_stop_color(offset='100%', color='white', opacity=None)
-    dwg.defs.add(rad_grad4)
-# установка узоров, пока заменены обратными градиентами    
-def set_patterns():
-    rad_grad1=svgwrite.gradients.RadialGradient(id="pat1")
-    rad_grad1.add_stop_color(offset='100%', color='rgb(230, 25, 25)', opacity=None)
-    rad_grad1.add_stop_color(offset='0%', color='white', opacity=None)
-    dwg.defs.add(rad_grad1)
-    
-    rad_grad2=svgwrite.gradients.RadialGradient(id="pat2")
-    rad_grad2.add_stop_color(offset='100%', color='rgb(50, 165, 80)', opacity=None)
-    rad_grad2.add_stop_color(offset='0%', color='white', opacity=None)
-    dwg.defs.add(rad_grad2)
-    
-    rad_grad3=svgwrite.gradients.RadialGradient(id="pat3")
-    rad_grad3.add_stop_color(offset='100%', color='rgb(20, 50, 250)', opacity=None)
-    rad_grad3.add_stop_color(offset='0%', color='white', opacity=None)
-    dwg.defs.add(rad_grad3)
-    
-    rad_grad4=svgwrite.gradients.RadialGradient(id="pat4")
-    rad_grad4.add_stop_color(offset='100%', color='rgb(0, 0, 0)', opacity=None)
-    rad_grad4.add_stop_color(offset='0%', color='white', opacity=None)
-    dwg.defs.add(rad_grad4)
+init_cardX=20 # значение самой левой карты
+gap=3 # промежуток между картами
+cardX=init_cardX+Card.card_widht*2+gap*2 # значение для текущей карты !!!
+cardY=30 #
+card_object=[] # массив хранящий экземпляры карт
+#    функция генерирует массив кодов, определяющих изображение на картах
+code_list=generate_set(feature_number=4,feature_variety=4)
+
 
 class Card: # класс карты, каждая карта будет уникальным объектом этого класса
     card_widht=183 #  ширина карты, из неё высчитывается высота карты и центры фигур
@@ -79,14 +27,16 @@ class Card: # класс карты, каждая карта будет уник
     def __init__(self):
         self.fill_color='rgb(00, 250, 00)' # цвет заливки фигуры по умолчанию - зеленый
         self.border_color='rgb(0, 200, 0)' # цвет контура фигуры по умолчанию - темно зеленый
-        self.gradient_color='url(#grad2)'  # цвет заливки по умолчанию - зеленый
-        self.pattern_color='url(#pat2)'
+        self.gradient_color='url(#grad2)'  # цвет градиента по умолчанию - зеленый
+        self.pattern_color='url(#pat2)' # цвет узора по умолчанию - зеленый
         self.figure_size=30 # размер фигуры -половина её длинны или радиус круга
  #       self.cardx, self.cardy=20, 30 # верхний левый угол карты
         self.card_height= self.card_widht//0.7 #  высота карты
 
+
 # методы смены цвета фигуры
-# сразу определяются цвета для контура фигуры, и если они будут, для заливки, градиента, узора        
+# с начала переопределяются цвета для контура фигуры, и если они будут, 
+# для заливки, градиента, узора. Если признаков 3 - остается цвет по умолчанию 
     def set_color1(self): # смена цвета на красный
         self.border_color='rgb(200, 0, 0)'
         self.fill_color='rgb(250, 0, 0)'
@@ -118,9 +68,12 @@ class Card: # класс карты, каждая карта будет уник
     def set_fill4(self): # смена типа заполнения на обратный градиент(заменить на узор)
         self.fill_type=self.pattern_color
 
-# методы задания колличества фигур, создает координаты для расположения центров этих фигур   
-# figure_centreX/Y массив из координат центров, цикл отрисовки фигур отрабатывает по числу координат в массиве X
-# координаты относительно края карты, Х по середине карты, Y зависит от числа фигур
+# методы задания колличества фигур, создает координаты для расположения 
+# центров этих фигур   
+# figure_centreX/Y кортеж из координат центров, цикл отрисовки фигур 
+# отрабатывает по числу координат в figure_centreX
+# координаты относительно края карты, Х по середине карты(или на 28% 72% ширины) 
+# Y зависит от числа фигур
     def set_position1(self): # центра единственной фигуры, Y по середины карты
         # кортеж из одного элемента нужно завершать запятой
         self.figure_centreX=(int(self.cardx+self.card_widht*0.5),)
@@ -146,8 +99,7 @@ class Card: # класс карты, каждая карта будет уник
                              int(self.cardy+self.card_height*0.65), 
                              int(self.cardy+self.card_height*0.35),
                              int(self.cardy+self.card_height*0.65))
-        
-        
+     
 # методы форм, собственно рисуют заданные фигуры, использует определенные выше переменные объекта для 
 # цвета(color), заполнения(fill), центра(figure_centreX) фигуры
     def draw_figure1(self): # рисование круга
@@ -157,7 +109,6 @@ class Card: # класс карты, каждая карта будет уник
             stroke=self.border_color,
             fill=self.fill_type,
             stroke_width=3))
-        
     def draw_figure2(self): # рисование креста, через функцию отрисовки многоугольника
         for i in range(len(self.figure_centreX)):
              dwg.add(dwg.polygon(points=Card.quarter_figure_expand(self.figure_centreX[i], self.figure_centreY[i],  
@@ -169,7 +120,6 @@ class Card: # класс карты, каждая карта будет уник
               stroke=self.border_color,
               fill=self.fill_type,
               stroke_width=3))    
-
     def draw_figure3(self): # рисование квадрата, через функцию отрисовки многоугольника
         for i in range(len(self.figure_centreX)):
              dwg.add(dwg.polygon(points=Card.quarter_figure_expand(self.figure_centreX[i], self.figure_centreY[i],  
@@ -179,7 +129,6 @@ class Card: # класс карты, каждая карта будет уник
               stroke=self.border_color,
               fill=self.fill_type,
               stroke_width=3))
-            
     def draw_figure4(self): # рисование восьмиконечной звезды, через функцию отрисовки многоугольника
          for i in range(len(self.figure_centreX)):
               dwg.add(dwg.polygon(points=Card.quarter_figure_expand(self.figure_centreX[i], self.figure_centreY[i],  
@@ -192,10 +141,10 @@ class Card: # класс карты, каждая карта будет уник
                fill=self.fill_type,
                stroke_width=3))   
     
- 
-    
-    def quarter_figure_expand(centerX, centerY, quart): # функция дорисовки четверти в полную фигуру
-# centerX, centerY центр фигуры, quart нижняя правая четверть отрисованной фигуры в виде массива точек
+# метод дорисовки четверти в полную фигуру
+# centerX, centerY центр фигуры, 
+# quart нижняя правая четверть отрисованной фигуры в виде массива точек
+    def quarter_figure_expand(centerX, centerY, quart):
         dots=len(quart) # число точек четверти
         sx,sy=0, -2 # множители для отрисовки
         d=0
@@ -213,22 +162,16 @@ class Card: # класс карты, каждая карта будет уник
             d-=2
             quart+=[(quart[d][0]+(quart[d][0]-centerX)*sx, quart[d][1]+(centerY-quart[d][1])*sy)]
         return quart
-    
-  
-    def draw_card_borders(self): # метод создания границ карты 
+ 
+# метод отрисовки границ карты
+    def draw_card_borders(self):  
         dwg.add(dwg.rect((self.cardx, self.cardy), (self.card_widht, self.card_height),  
         stroke=svgwrite.rgb(10, 10, 16, '%'),
         fill='none'))
         
-        
-init_cardX=20 # значение самой левой карты
-gap=3 # промежуток между картами
-cardX=init_cardX+Card.card_widht*2+gap*2 # значение для текущей карты !!!
-cardY=30 #
-card_object=[] # массив хранящий экземпляры карт
-code_list=generate_set()
+      
+
 for i in range(len(code_list)):
-    
     card_object.append(Card())
 # определение расположения карты на листе формата A4
 # определение X исходя из горионтальной позиции    
@@ -247,10 +190,9 @@ for i in range(len(code_list)):
         dwg.save()
         file_path=file_path.replace( str(i//9-1)+'.svg', str(i//9)+'.svg')
         dwg = svgwrite.Drawing(file_path, size=(u'100%', u'100%'))
-        set_gradients()
-        set_patterns()
-        
-        
+# для каждого файла нового файла требуется пересоздание градиентов и паттернов        
+        set_gradients(dwg)
+        set_patterns(dwg)
         
 # сохранение полученных координат 
     card_object[i].cardx= cardX 
@@ -278,7 +220,6 @@ for i in range(len(code_list)):
         card_object[i].set_fill3()
     if code[-3]=='4':
         card_object[i].set_fill4()
-        
 # второй с права регистр - число фигур         
     if code[-2]=='1':
         card_object[i].set_position1()
@@ -300,5 +241,4 @@ for i in range(len(code_list)):
 # отрисовка границ карты        
     card_object[i].draw_card_borders()
   
-    
 dwg.save()   
